91a17f928d80ac7fbff962df071742ef6aee9597
ahnjongin <goodtime1998@naver.com>
2021-08-02 12:11:44 +0900

    To-Do-List

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 const clock = document.querySelector("h2#clock");
-
+const dates=document.querySelector("h2#date");
 function getClock(){
   const date= new Date();
   const hours = String(date.getHours()).padStart(2,"0");
@@ -7,6 +7,14 @@ function getClock(){
   const seconds = String(date.getSeconds()).padStart(2,"0");
   clock.innerText=`${hours}:${minutes}:${seconds}`;
 }
+function getDate(){
+  const dated= new Date();
+  const year= String(dated.getFullYear()).padStart(2,"0");
+  const month= String(dated.getMonth()+1).padStart(2,"0");
+  const day=String(dated.getDate()).padStart(2,"0");
+  dates.innerText=`${year}.${month}.${day}`;
+}
 
+getDate()
 getClock()
 setInterval(getClock,1000);
