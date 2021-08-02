a1960b40bf38a3357c78533daebe0d4452aef5e6
ahnjongin <goodtime1998@naver.com>
2021-08-02 11:05:30 +0900

    To-Do-List

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ const city=document.querySelector("#weather span:last-child");
 function onGeoOk(position){
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url).then((response) => response.json())
   .then((data)=>{
     city.innerText=data.name;
